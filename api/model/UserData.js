9ee99b4b8392bd5791c37e98d0bd35399e70211d
NavCore-1 <ssurajvenkat@protonmail.com>
2024-08-30 22:58:40 +0530

    Add files via upload

---
+++ api/model/UserData.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const UserSchema=mongoose.Schema({
+    Name:String,
+    Place:String,
+    Age:String,
+    Password:String,
+    Email:String,
+    phoneNumber:String,
+    Type:String,
+})
+
+const UserData=mongoose.model('userdetail',UserSchema)
+module.exports=UserData
